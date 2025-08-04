3cca2fdb45bf4deef10730fdaeec08a941b954f4
SaidOspina <deislersaid1418@gmail.com>
2025-08-04 14:18:16 -0500

    ft:Upload final update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Autor: Deisler Said Prado Ospina</p>
+      <p>Autor: <a href="https://github.com/SaidOspina/actividadIntegrada.git" target="_blank">Deisler Said Prado Ospina</a></p>
     </footer>
   );
 };
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -12,24 +12,32 @@ const Navbar = () => {
     <nav className="navbar">
       <ul>
         <li>
-          <Link to="/" className={isActive('/')}>
-            Inicio
-          </Link>
+            <h2>
+                <Link to="/" className={isActive('/')}>
+                    Inicio
+                </Link>
+            </h2>
         </li>
         <li>
-          <Link to="/students" className={isActive('/students')}>
-            Estudiantes
-          </Link>
+            <h2>
+                <Link to="/students" className={isActive('/students')}>
+                    Estudiantes
+                </Link>
+            </h2>
         </li>
         <li>
-          <Link to="/qr" className={isActive('/qr')}>
-            QR
-          </Link>
+            <h2>
+                <Link to="/qr" className={isActive('/qr')}>
+                    QR
+                </Link>
+            </h2>
         </li>
         <li>
-          <Link to="/error" className={isActive('/error')}>
-            Error
-          </Link>
+            <h2>
+                <Link to="/error" className={isActive('/error')}>
+                    Error
+                </Link>
+            </h2>
         </li>
       </ul>
     </nav>
