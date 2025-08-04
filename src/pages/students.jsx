7885e11d9e2001f21e3d50e84066a079f4bd75c3
SaidOspina <deislersaid1418@gmail.com>
2025-08-04 14:20:38 -0500

    ft:Upload dist

--- src/pages/students.jsx
+++ src/pages/students.jsx
@@ -26,22 +26,22 @@ const Students = () => {
     },
     {
       id: 4,
-      name: "María García",
-      role: "Estudiante de Frontend",
+      name: "María Romero",
+      role: "Estudiante Analista",
       description: "Apasionada por el desarrollo.",
       profileImage: "https://res.cloudinary.com/dqaxadodm/image/upload/v1754333101/istockphoto-1194465593-612x612_igqaho.jpg"
     },
     {
       id: 5,
-      name: "Carlos López",
-      role: "Estudiante de Backend",
+      name: "Carlos Ospina",
+      role: "Estudiante de BD",
       description: "Enfocado en el desarrollo personal.",
       profileImage: "https://res.cloudinary.com/dqaxadodm/image/upload/v1754333238/ai-generated-portrait-of-a-young-man-no-facial-expression-facing-the-camera-isolated-white-background-ai-generative-free-photo_uha1di.jpg"
     },
     {
       id: 6,
-      name: "Ana Rodríguez",
-      role: "Estudiante de Full Stack",
+      name: "Ana Reina",
+      role: "Estudiante de Backend",
       description: "Hay que ser versátil y proactivo.",
       profileImage: "https://res.cloudinary.com/dqaxadodm/image/upload/v1754333172/istockphoto-1194465573-612x612_co8qgb.jpg"
     }
